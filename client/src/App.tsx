7a82bc8870135340e1ca0efc8eba051841654af9
Haile-08 <haile4cmd@gmail.com>
2023-10-09 09:32:47 +0300

    Setup react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  RouterProvider,
+  createBrowserRouter,
+  useRouteError,
+} from "react-router-dom";
+import Home from "./Components/Home/Home";
+import MainLayout from "./Components/MainLayout/MainLayout";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  function ErrorPage() {
+    const error: any = useRouteError();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    return (
+      <div id="error-page">
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          <i>{error.statusText || error.message}</i>
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <RouterProvider router={router} />
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
